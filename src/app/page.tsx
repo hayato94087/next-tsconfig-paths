1c57fa700047da2d27fea80f42b4fd3efdacc6ef
hayato94087 <hayato94087@gmail.com>
2024-02-02 02:43:58 +0900

    エイリアスを@/から@/componentsに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import HelloWorldButton from "@/components/hello-world-button";
+import HelloWorldButton from "@components/hello-world-button";
 import { type FC } from "react";
 
 const Home: FC = () => {
